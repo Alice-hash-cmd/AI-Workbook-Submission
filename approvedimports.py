0a4b876207b4da11dd3d04e259581318db0c8ed5
Alice-hash-cmd <thapaalice543@gmail.com>
2025-05-15 21:01:13 +0545

    Add files via upload

--- approvedimports.py
+++ approvedimports.py
@@ -1,3 +1,12 @@
+from importlib import reload
+
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, LabelBinarizer
+from sklearn.model_selection import train_test_split 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from warnings import simplefilter
+from sklearn.exceptions import ConvergenceWarning
+simplefilter("ignore", category=ConvergenceWarning)

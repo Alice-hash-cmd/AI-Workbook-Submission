ff1c739d05763cfafdd23c690bc9f19cd6ffea16
Alice-hash-cmd <thapaalice543@gmail.com>
2025-05-15 20:57:43 +0545

    Add files via upload

--- approvedimports.py
+++ approvedimports.py
@@ -1,13 +1,3 @@
-## File approvedimports.py
-#Author: Jim Smith 2025
-#contains the packagges thatamy be imported. 
-# All  import statements are blocked on the marking server 
-# except "from approvedimports import *"
-
-
 import numpy as np
-from candidatesolution import CandidateSolution
-from problem import Problem
-from combinationproblem import CombinationProblem
-from singlemembersearch import *
-from maze import Maze
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
